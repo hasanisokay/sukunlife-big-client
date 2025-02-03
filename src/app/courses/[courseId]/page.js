0ca1e20dad05112e0b6e5ce1546e3fcb2cf33ff0
hasanisokay <hasanisokay@gmail.com>
2025-02-03 16:23:28 +0600

    added course certificate

--- src/app/courses/[courseId]/page.js
+++ src/app/courses/[courseId]/page.js
@@ -10,6 +10,7 @@ const singleCoursePage = async ({ params }) => {
     const p = await params;
     const courseId = p.courseId;
     let course = await getCoursePublic(courseId);
+    console.log(course)
     if (course?.status === 200) {
       return <SingleCoursePage course={course?.course} />;
     } else return <NotFound />;
@@ -26,7 +27,7 @@ export async function generateMetadata({ params }) {
     const p = await params;
     const courseId = p.courseId;
     let courseData = await getCoursePublic(courseId);
-    const course = courseData.course;
+    const course = courseData?.course;
     const courseCoverUrl = `${host}${blogCover.src}`;
 
     const description = course?.description
@@ -36,7 +37,7 @@ export async function generateMetadata({ params }) {
     let metadata = {
       title: `${course?.title} - ${websiteName}`,
       description:
-        course.seoDescription ||
+        course?.seoDescription ||
         description ||
         "Detailed description of the course.",
       keywords: ["সুকুনলাইফ কোর্স"],
@@ -44,24 +45,24 @@ export async function generateMetadata({ params }) {
       canonical: `${host}/courses/${courseId}`,
     };
     if (course) {
-      if (course.tags > 2) {
-        const keywords = course.tags.split(",");
+      if (course?.tags > 2) {
+        const keywords = course?.tags.split(",");
         metadata.keywords.push(...keywords);
       } else {
-        const titleKeywords = course.title
+        const titleKeywords = course?.title
           .split(" ")
           .filter((kw) => kw.length > 3);
         metadata.keywords.push(...titleKeywords);
       }
       metadata.other = {
-        "twitter:image": course.coverPhotoUrl || courseCoverUrl || "",
+        "twitter:image": course?.coverPhotoUrl || courseCoverUrl || "",
         "twitter:card": "summary_large_image",
         "twitter:title": metadata.title,
         "twitter:description": metadata.description,
         "og:title": metadata.title,
         "og:description": metadata.description,
         "og:url": `${host}/courses/${courseId}`,
-        "og:image": course.coverPhotoUrl || courseCoverUrl || "",
+        "og:image": course?.coverPhotoUrl || courseCoverUrl || "",
         "og:type": "article",
         "og:site_name": websiteName,
         "og:locale": "bn_BD",
--- src/components/courses/SingleCoursePage.jsx
+++ src/components/courses/SingleCoursePage.jsx
@@ -6,14 +6,11 @@ import StarRating from '../rating/StarRating';
 import Link from 'next/link';
 import formatDate from '@/utils/formatDate.mjs';
 import CoursePrice from './CoursePrice';
-import { StatsSVG, UpdateSVG } from '../svg/SvgCollection';
+import { CertificateSVG, StatsSVG, UpdateSVG } from '../svg/SvgCollection';
 
 const SingleCoursePage = ({ course }) => {
     console.log(course)
-    const [expandedModule, setExpandedModule] = useState(null);
-    const toggleModule = (index) => {
-        setExpandedModule(expandedModule === index ? null : index);
-    };
+
     const overlayVariants = {
         hidden: { opacity: 0, y: 10 },
         visible: { opacity: 1, y: 0, transition: { duration: 0.3, ease: "easeOut" } },
@@ -38,11 +35,16 @@ const SingleCoursePage = ({ course }) => {
                     <StarRating ratingCount={course.reviewsCount} totalRating={course.ratingSum} />
                 </Link>
                 <div className="mt-4 flex items-center gap-2 text-sm text-gray-800 dark:text-gray-200">
+                    <CertificateSVG /> <p className='font-semibold'>
+                        Certificate of completion</p>
+                </div>
+                <div className="mt-4 flex items-center gap-2 text-sm text-gray-800 dark:text-gray-200">
                     <UpdateSVG /> <p className='font-semibold'>Last Updated: {formatDate(course.updatedOn || course.addedOn)}</p>
                 </div>
                 <div className="mt-4 flex items-center gap-2 text-sm text-gray-800 dark:text-gray-200">
                     <StatsSVG /> <p className='font-semibold'>Total Enrolled: {course.studentsCount}</p>
                 </div>
+
             </motion.div>
             <motion.div
                 className="shadow-lg p-5 my-4 max-w-4xl  bg-white dark:bg-gray-800 border"
@@ -54,11 +56,11 @@ const SingleCoursePage = ({ course }) => {
                 <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
                     What You'll Learn
                 </h3>
-                <ul className="mt-3 grid grid-cols-1  md:grid-cols-2 text-gray-600 dark:text-gray-300 text-sm">
+                <ul className="mt-3 grid grid-cols-1  md:grid-cols-2 gap-2 text-gray-600 dark:text-gray-300 text-sm">
                     {course.learningItems?.length > 0 ? (
                         course?.learningItems?.map((item, index) => (
                             <li key={index} className="flex items-center">
-                                <span className="text-green-500 mr-2 text-[14px]">		&#10003;</span> {item.text}
+                                <span className="text-green-500 mr-2 text-sm">&#10003;</span> {item.text}
                             </li>
                         ))
                     ) : (
@@ -69,36 +71,7 @@ const SingleCoursePage = ({ course }) => {
             </motion.div>
             {/* </motion.div> */}
             {/* Modules */}
-            <div className="mt-4">
-                {course.modules.map((module, index) => (
-                    <div key={index} className="border-b border-gray-300 dark:border-gray-700 py-2">
-                        <button
-                            className="w-full text-left flex justify-between items-center py-2 font-medium text-lg"
-                            onClick={() => toggleModule(index)}
-                        >
-                            {module.title}
-                            <span>{expandedModule === index ? '−' : '+'}</span>
-                        </button>
-                        <AnimatePresence>
-                            {expandedModule === index && (
-                                <motion.div
-                                    initial={{ opacity: 0, height: 0 }}
-                                    animate={{ opacity: 1, height: 'auto' }}
-                                    exit={{ opacity: 0, height: 0 }}
-                                    className="overflow-hidden"
-                                >
-                                    {module.items.map((item, i) => (
-                                        <div key={i} className="p-2 pl-4 border-l border-gray-400 dark:border-gray-600">
-                                            <h4 className="text-md font-semibold">{item.title}</h4>
-                                            {item.status === 'public' && <p className="text-gray-600 dark:text-gray-300">{item.description}</p>}
-                                        </div>
-                                    ))}
-                                </motion.div>
-                            )}
-                        </AnimatePresence>
-                    </div>
-                ))}
-            </div>
+
             <div id='reviews'>
                 rating showing area
             </div>
