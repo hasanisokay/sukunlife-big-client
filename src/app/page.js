cab03a7d9d0493b5f1d99ed210634e493c2d4677
hasanisokay <hasanisokay@gmail.com>
2025-11-29 22:07:26 +0600

    added caching on home

--- src/app/page.js
+++ src/app/page.js
@@ -4,30 +4,25 @@ import getAllBlog from "@/utils/getAllBlog.mjs";
 import getTopCourses from "@/utils/gettopCourses.mjs";
 import getTopReviews from "@/utils/getTopReviews.mjs";
 
-
-
 const page = async () => {
   try {
-    const [
-      topProductsRes,
-      topReviews,
-      topCourses,
-      recentBlogs,
-    ] = await Promise.all([
-      fetch(`${SERVER}/api/public/top-sold-items?limit=3`, 
-    ).then((res) => res.json()),
-      getTopReviews(),
-      getTopCourses(),
-      getAllBlog(1, 3),
-    ]);
+    const [topProductsRes, topReviews, topCourses, recentBlogs] =
+      await Promise.all([
+        fetch(`${SERVER}/api/public/top-sold-items?limit=3`, {
+          next: { revalidate: 3600 },
+        }).then((res) => res.json()),
+        getTopReviews(),
+        getTopCourses(),
+        getAllBlog(1, 3),
+      ]);
 
     const topProducts =
       topProductsRes?.status === 200 ? topProductsRes?.data : [];
     return (
       <Homepage
-      recentBlogs={recentBlogs?.blogs}
-      topCourses={topCourses?.courses}
-      appointmentReviews={topReviews?.appointmentReviews}
+        recentBlogs={recentBlogs?.blogs}
+        topCourses={topCourses?.courses}
+        appointmentReviews={topReviews?.appointmentReviews}
         topProducts={topProducts}
         shopReviews={topReviews?.shopReviews}
         courseReviews={topReviews?.courseReviews}
