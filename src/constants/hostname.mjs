fe74c65a545a27c4978937e9fbc35e140b0966fe
hasanisokay <hasanisokay@gmail.com>
2026-01-19 14:53:27 +0600

    changed hostname

--- src/constants/hostname.mjs
+++ src/constants/hostname.mjs
@@ -1,6 +1,7 @@
 // import { headers } from "next/headers";
 
 const hostname = async () => {
+  return 'https://sukunlife.vercel.app'
   // const headerList = headers();
   // const host = (await headerList).get("host") || 'localhost:3000';
   const env = process.env.NODE_ENV;
