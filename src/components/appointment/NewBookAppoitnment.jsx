d5b2e9fcbb78bb556805e34eb548c83b1da8ca8c
hasanisokay <hasanisokay@gmail.com>
2026-01-23 16:07:36 +0600

    changed appointment tk to 500

--- src/components/appointment/NewBookAppoitnment.jsx
+++ src/components/appointment/NewBookAppoitnment.jsx
@@ -362,7 +362,7 @@ const NewBookAppointment = ({ preSelectedService }) => {
                                                 : "bg-gray-100 text-gray-700 hover:bg-gray-200 dark:bg-gray-700 dark:text-gray-300 dark:hover:bg-gray-600"
                                                 }`}
                                         >
-                                            Pay 20 TK Now
+                                            Pay 500 TK Now
                                         </button>
                                         <button
                                             type="button"
