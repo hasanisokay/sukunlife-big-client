a0bc69c52d62a9303c8ca3ba420469dd8e5666a7
hasanisokay <hasanisokay@gmail.com>
2026-02-09 23:36:26 +0600

    changed book appointment modal css

--- src/components/appointment/NewBookAppoitnment.jsx
+++ src/components/appointment/NewBookAppoitnment.jsx
@@ -295,7 +295,7 @@ useEffect(() => {
             <WhyChoosSukunLIfeBookingAppointment />
 
             <div>
-                <div className="mx-auto flex w-72 items-center justify-center">
+                <div className="mx-auto flex w-full items-center justify-center">
                     <div
                         className={`fixed z-[100] flex items-center justify-center ${openModal ? 'opacity-1 visible' : 'invisible opacity-0'
                             } inset-0 bg-black/20 backdrop-blur-sm duration-100`}
